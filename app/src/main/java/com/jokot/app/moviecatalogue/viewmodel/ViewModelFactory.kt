31d8514f71cdab9b79022c2ba5f98a3f0f86e621
joko triyanto <jokotriyanto123@outlook.com>
2021-05-25 23:21:58 +0700

    add view model factory

---
+++ app/src/main/java/com/jokot/app/moviecatalogue/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,44 @@
+package com.jokot.app.moviecatalogue.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.jokot.app.moviecatalogue.data.FilmRepository
+import com.jokot.app.moviecatalogue.di.Injection
+import com.jokot.app.moviecatalogue.ui.detail.movie.DetailMovieViewModel
+import com.jokot.app.moviecatalogue.ui.detail.tv.DetailTvShowViewModel
+import com.jokot.app.moviecatalogue.ui.movie.MovieViewModel
+import com.jokot.app.moviecatalogue.ui.tv.TvShowViewModel
+
+class ViewModelFactory private constructor(private val mFilmRepository: FilmRepository): ViewModelProvider.NewInstanceFactory(){
+
+    companion object {
+        @Volatile
+        private var instance: ViewModelFactory? = null
+
+        fun getInstance(): ViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: ViewModelFactory(Injection.provideRepository()).apply {
+                    instance = this
+                }
+            }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(MovieViewModel::class.java) -> {
+                MovieViewModel(mFilmRepository) as T
+            }
+            modelClass.isAssignableFrom(TvShowViewModel::class.java) -> {
+                TvShowViewModel(mFilmRepository) as T
+            }
+            modelClass.isAssignableFrom(DetailMovieViewModel::class.java) -> {
+                DetailMovieViewModel(mFilmRepository) as T
+            }
+            modelClass.isAssignableFrom(DetailTvShowViewModel::class.java) -> {
+                DetailTvShowViewModel(mFilmRepository) as T
+            }
+            else -> throw Throwable("Unknown ViewModel class: " + modelClass.name)
+        }
+    }
+}
